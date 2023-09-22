d8f3418fd96ee96ecf608b6dc1e5be2c14560eb5
Leluto Deku <hammadsiddiqui1212@gmail.com>
2023-09-22 21:07:38 +0530

    my own code for pattern-12

---
+++ striver_DSA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()	{
+
+
+	for(int i=1; i<5; i++)	{
+		for(int j=1; j<=i; j++)
+			cout<<j;
+		
+		for(int j=1; j<2*(5-i);j++)
+			cout<<" ";
+
+		for(int j=i; j>=1; j--)
+			cout<<j;
+
+		cout<<endl;
+	}
+	return 0;
+
+}
