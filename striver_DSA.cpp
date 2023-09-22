915ea0e35eff5dbfed1520b77097615c7967d5d2
Leluto Deku <hammadsiddiqui1212@gmail.com>
2023-09-22 22:19:44 +0530

    code for pattern-22

--- striver_DSA.cpp
+++ striver_DSA.cpp
@@ -3,19 +3,35 @@
 using namespace std;
 int main()	{
 
-
-	for(int i=1; i<5; i++)	{
-		for(int j=1; j<=i; j++)
-			cout<<j;
+	// a pattern
+	// for(int i=1; i<5; i++)	{
+	// 	for(int j=1; j<=i; j++)
+	// 		cout<<j;
 		
-		for(int j=1; j<2*(5-i)-1;j++)
-			cout<<" ";
+	// 	for(int j=1; j<2*(5-i)-1;j++)
+	// 		cout<<" ";
 
-		for(int j=i; j>=1; j--)
-			cout<<j;
+	// 	for(int j=i; j>=1; j--)
+	// 		cout<<j;
 
-		cout<<endl;
-	}
+	// 	cout<<endl;
+	// }
+
+	//another pattern
+	// for(int i=0; i<2*4-1;i++)	{
+	// 	for(int j=0;j<2*4-1;j++)	{
+	// 		int top = i;
+	// 		int left = j;
+	// 		int right = 2*4 - 2 -j;
+	// 		int bottom = 2*4 - 2 -i;
+
+	// 		cout<<(4 - min(min(top,bottom), min(left,right)));
+	// 	}
+
+	// 	cout<<endl;
+	// }
+
+
 	return 0;
 
 }
